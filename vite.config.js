f89103166b53c39903ca0ec9e8e4761c98930b03
Yoshida <61533915+naoto-ysd@users.noreply.github.com>
2024-12-25 01:14:20 +0900

    change config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: "0.0.0.0", // 外部アクセスを許可
+    port: 5173,
+  },
 })
