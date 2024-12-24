f45dea64a19b6c91c842b517028d2c301a41ada3
Yoshida <61533915+naoto-ysd@users.noreply.github.com>
2024-12-25 01:14:54 +0900

    create entrypoint

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,47 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1>APIから取得したデータ一覧</h1>
+    <div v-if="loading">Loading...</div>
+    <div v-else-if="error">{{ error }}</div>
+    <ul v-else>
+      <li v-for="item in items" :key="item.id">
+        {{ item.name }}
+      </li>
+    </ul>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<script>
+import { ref, onMounted } from "vue";
+import axios from "axios";
+
+export default {
+  setup() {
+    const items = ref([]);
+    const loading = ref(true);
+    const error = ref(null);
+
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("https://api.example.com/items");
+        items.value = response.data;
+      } catch (err) {
+        error.value = "データの取得に失敗しました。";
+      } finally {
+        loading.value = false;
+      }
+    };
+
+    onMounted(fetchData);
+
+    return {
+      items,
+      loading,
+      error,
+    };
+  },
+};
+</script>
+
+<style>
 </style>
