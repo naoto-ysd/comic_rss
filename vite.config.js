c37b39bacae10e41ed2bbe0b140e1f17d9b53823
Yoshida <61533915+naoto-ysd@users.noreply.github.com>
2025-06-15 05:39:32 +0900

    initialize

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    host: "0.0.0.0", // 外部アクセスを許可
-    port: 5173,
-  },
+  plugins: [react()],
 })
